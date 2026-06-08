08f60ebc44e5c17c2f008006cf786bfd90fa1a66
asiyasa <asiyasa07@gmail.com>
2026-06-08 11:25:23 +0530

    update axios base URL to relative path

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,17 @@
+// import axios from 'axios';
+
+// // Axios Instance உருவாக்கி அதிலேயே Base URL-ஐ காமனாக செட் செய்கிறோம்
+// const API = axios.create({
+//     baseURL: 'http://localhost:5000/api/todo'
+// });
+
+// export default API;
 import axios from 'axios';
 
-// Axios Instance உருவாக்கி அதிலேயே Base URL-ஐ காமனாக செட் செய்கிறோம்
+// baseURL-ஐ வெறும் '/' என்று கொடுத்தால், அது தற்போது ஆப் இயங்கும் 
+// அதே URL-ஐ (Render URL) எடுத்துக்கொள்ளும்.
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api/todo'
+    baseURL: '/api/todo'
 });
 
 export default API;
